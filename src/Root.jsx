aec0357416272f6290efb0cc08102480c1615539
Min Latt <minminlaxz@gmail.com>
2021-07-19 14:21:11 +0630

    Mounting associated pages

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,14 +1,27 @@
 // this is a socket for all pages
 import React from 'react';
+import { Route } from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom";
 
+// context providers
+import { GistContextProvider } from './contexts/GistContext';
+
+// components and pages
 import Main from './components/Main/Main';
-import Routes from './routes';
+import Home from './pages/Home/Home';
+import Gists from './pages/Gists/Gists';
+import Iots from './pages/Iots/Iots';
 const Root = () => {
     return (
-        <>
-            <Main /> {/* <-- this is main header */}
-            <Routes /> {/* <-- this is all routes */}
-        </>
+        <Router>
+            <Main />
+
+            <Route exact path="/" component={Home} />
+            <GistContextProvider>
+                <Route exact path="/gists" component={Gists} />
+            </GistContextProvider>
+            <Route exact path="/iots" component={Iots} />
+        </Router>
     );
 }
 
