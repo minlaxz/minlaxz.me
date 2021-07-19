58b413f97fd8f905399ab95d278890f815213ffb
Min Latt <minminlaxz@gmail.com>
2021-07-20 01:37:35 +0630

    New component from HOME

---
+++ src/components/IconsMetrics/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import IconsWithMetrics from './IconsWithMetrics';
+
+const IconMetrics = () => {
+    return (
+        <div className="container">
+            <IconsWithMetrics /> {/* includes two rows */}
+        </div>
+    );
+}
+
+export default IconMetrics;
