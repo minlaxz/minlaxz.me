5a4798e85e5580747d7c502673fdf9972fdd5ad4
Min Latt <minminlaxz@gmail.com>
2021-07-20 22:23:59 +0630

    Sample component to test dark mode
    Maybe i am removing materialize css

---
+++ src/components/Sample/Sample.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Sample = () => {
+    return (
+        <>
+            <div className="px-4 mx-auto max-w-screen-sm md:max-w-screen-md md:p-0 lg:max-w-screen-lg xl:max-w-screen-xl">
+                <div className="min-h-screen flex justify-center items-center bg-white dark:bg-gray-800">
+                    <h1 className="text-gray-900 dark:text-white text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-8">
+                        Dark Mode
+                    </h1>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default Sample;
