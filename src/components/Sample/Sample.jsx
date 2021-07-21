c9810f3f51eee4dd57007f3e51323492d834e531
Min Latt <minminlaxz@gmail.com>
2021-07-21 21:21:12 +0630

    Sample component to test dark mode

--- src/components/Sample/Sample.jsx
+++ src/components/Sample/Sample.jsx
@@ -7,7 +7,7 @@ const Sample = () => {
             <div className="px-4 mx-auto max-w-screen-sm md:max-w-screen-md md:p-0 lg:max-w-screen-lg lg:p-0 xl:max-w-screen-xl xl:p-0">
                 <div className="min-h-screen flex justify-center items-center">
                     <h1 className="text-gray-900 dark:text-white text-3xl sm:text-5xl lg:text-6xl leading-none font-extrabold tracking-tight mb-8">
-                        Dark Mode
+                        Persistent Dark Mode
                     </h1>
                 </div>
             </div>
