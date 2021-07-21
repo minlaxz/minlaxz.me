84572909c774290c9cc8e0630eb4bc0f9eb43962
Min Latt <minminlaxz@gmail.com>
2021-07-21 21:22:35 +0630

    Using react-icons

--- src/components/ThemeToggler/ThemeToggler.jsx
+++ src/components/ThemeToggler/ThemeToggler.jsx
@@ -9,14 +9,14 @@ const Toggle = () => {
             {theme === "dark" ? (
                 <HiSun
                     onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                    className="text-gray-500 dark:text-gray-400 text-2xl cursor-pointer"
+                    className="text-yellow-500 dark:text-yellow-500 text-2xl cursor-pointer"
                 />
             ) : (
-                <HiMoon
+                <HiMoon 
                     onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-                    className="text-gray-500 dark:text-gray-400 text-2xl cursor-pointer"
+                    className="text-gray-700 dark:text-gray-400 text-2xl cursor-pointer"
                 />
-            )}
+            )}  <p className="text-xs text-yellow-700 dark:text-white"> ☝ Click this dude 🤣</p>
         </div>
     );
 };
