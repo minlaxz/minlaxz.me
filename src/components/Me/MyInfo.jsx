9fca55fc290e512f7c16d107d3b81fc12a94df5f
Min Latt <minminlaxz@gmail.com>
2021-07-21 21:20:55 +0630

    Used grid on Me components

--- src/components/Me/MyInfo.jsx
+++ src/components/Me/MyInfo.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 
-const MyInfo = ({gists}) => {
+const MyInfo = ({ gists }) => {
     return (
-        <div className="col-start-2 col-end-12">
-            <p className="card-head" > "About : " </p>
-            <div className="card-body" >
-                <b
-                    key="name"
-                    style={{ color: "#FECB2E", fontWeight: "bolder" }}
-                > Min Latt </b>
-                    " from "
-                    <a key="mmwiki" href="https://en.wikipedia.org/wiki/Myanmar"> Myanmar </a>, " 🇲🇲 (Burma).",
-
-                    <ul key="ul_bg">
-                    <li key="ttu">Background : B.E(EC) <a href="http://www.ttu.edu.mm/">(@TTU)</a> </li>
-                    <li key="devops"> I know <a href="https://docker.com">Docker</a>, <a href="https://www.heroku.com">Heroku</a> and Some..</li>
-                    <li key="github"> My super duper hub <a href="https://github.com/minlaxz"> github/minlaxz </a></li>
-                    <li key="gists">I have <a href="https://gist.github.com/minlaxz"><span style={{ color: "red" }}>{gists.length}</span> gists</a> ( =&gt; react context.)</li>
+        <div className="col-start-1 col-end-13 md:col-start-2 md:col-end-13 lg:col-start-6 lg:col-end-12 xl:col-start-2 xl:col-end-13 2xl:col-start-5 2xl:col-end-13">
+            <p className="font-sans font-bold text-xl xl:text-2xl text-black dark:text-indigo-600" > "About : " </p>
+            <div className="text-base md:text-lg lg:text-xl xl:text-2xl">
+                <ul key="ul_bg">
+                    <li key="name" className="dark:text-white">
+                        <span className="font-sans font-bold text-yellow-500 dark:text-yellow-400"> Min Latt </span> from "
+                        <a key="mmwiki" href="https://en.wikipedia.org/wiki/Myanmar" className="underline text-blue-500 dark:text-purple-500" > Myanmar" 🇲🇲 (Burma).</a>
+                    </li>
+                    <li key="ttu" className="dark:text-white">Background : B.E(EC) <a href="http://www.ttu.edu.mm/" className="underline text-blue-500 dark:text-purple-500">(@TTU)</a> </li>
+                    <li key="devops" className="dark:text-white" > I know <a href="https://docker.com" className="underline text-blue-500 dark:text-purple-500">Docker</a>, <a href="https://www.heroku.com" className="underline text-blue-500 dark:text-purple-500">Heroku</a> and Some..</li>
+                    <li key="github" className="dark:text-white"> My super duper hub <a href="https://github.com/minlaxz" className="underline text-blue-500 dark:text-purple-500"> github/minlaxz </a></li>
+                    <li key="gists" className="dark:text-white" >I have <a href="https://gist.github.com/minlaxz" className="underline text-blue-500 dark:text-purple-500"><span style={{ color: "red" }}>{gists.length}</span> gists</a> ( =&gt; react context.)</li>
                     {/* <div class="container-fluid justify-content-end">
                         <a href="#" >Gists</a>
                     </div> */}
-                    <li key="thisapp">This website is maintained <a href="https://github.com/minlaxz/minlaxz.me">@minlaxz/minlaxz.me</a>.</li>
+                    <li key="thisapp" className="dark:text-white">This website is maintained <a href="https://github.com/minlaxz/minlaxz.me" className="underline text-blue-500 dark:text-purple-500">@minlaxz/minlaxz.me</a>.</li>
 
-                    <p>When i push to "main" branch github workflows will automatically deploy to firebase for me.</p>
-                    <p>Also when i PR to "main" branch flows will deploy for the preview <a href="https://github.com/minlaxz/minlaxz-dev/tree/main/.github/workflows">Checkout here</a></p> 
+                    <p className="text-gray-700 dark:text-gray-400 text-sm md:text-base">When i push to "main" branch github workflows will automatically deploy to firebase for me.</p>
+                    <p className="text-green-500 dark:text-green-700 text-sm md:text-base">Also when i PR to "main" branch flows will deploy for the preview <a href="https://github.com/minlaxz/minlaxz-dev/tree/main/.github/workflows" className="underline text-blue-500 dark:text-purple-500">Checkout here</a></p>
                 </ul>
             </div>
         </div>
--- src/components/Me/index.jsx
+++ src/components/Me/index.jsx
@@ -1,17 +1,30 @@
 import React from "react";
-import M from "materialize-css";
+// import M from "materialize-css";
 
 import { GistContext } from "../../contexts/GistContext";
 import { storage } from "../../services/Firebase";
 
 import MyPicture from "./MyPicture";
 import MyInfo from "./MyInfo";
+// import myimage from "../../assets/itssssme.jpg";
 
-const HandleAfterDocumentLoaded = () => {
-    const elems = document.querySelectorAll(".materialboxed");
-    M.Materialbox.init(elems, { inDuration: 500, outDuration: 500 });
-};
+// const HandleAfterDocumentLoaded = () => {
+//     const elems = document.querySelectorAll(".materialboxed");
+//     M.Materialbox.init(elems, { inDuration: 500, outDuration: 500 });
+// };
 
+const Temp = () => {
+    return (
+        <>
+            <p className="text-center text-sm md:text-base text-pink-500 dark:text-pink-700">
+                This is preview and active development.
+                Every stable pull request to 'main' is going to be automatically built and deployed at
+                <a href="https://minlaxz.me" target="_blank" rel="noopener noreferrer" className="underline text-blue-600 dark:text-blue-800">minlaxz.me</a>
+                by github actions</p>
+            <pre className="text-xs text-center text-red-400 dark:text-red-800">Navigation dropdown is a bug.</pre>
+        </>
+    )
+}
 const Me = () => {
     const gists = React.useContext(GistContext);
     const [imageUrl, setImageUrl] = React.useState(null);
@@ -21,14 +34,19 @@ const Me = () => {
             .then((url) => {
                 setImageUrl(url);
             })
-        HandleAfterDocumentLoaded();
+        // HandleAfterDocumentLoaded();
     }, [storageRef]);
 
     return (
-        <div className="grid grid-cols-12 gap-4" style={{ marginTop: "4%" }}>
-            <MyPicture imageUrl={imageUrl} />
-            <MyInfo gists={gists} />
+        <div className="px-4 mx-auto max-w-screen-sm md:max-w-screen-md md:p-0 lg:max-w-screen-lg lg:p-0 xl:max-w-screen-xl xl:p-0">
+            <Temp />
+            <div className="min-h-screen grid justify-center items-center grid-cols-12 gap-4 md:grid-cols-12 md:gap-6 lg:grid-cols-12 lg:gap-6 xl:grid-cols-12 xl:gap-6 2xl:grid-cols-12 2xl:gap-6">
+                <MyPicture imageUrl={imageUrl} />
+                <MyInfo gists={gists} />
+                {/* <MyInfo gists={{ gists: { length: 22 } }} /> */}
+            </div>
         </div>
+
     );
 };
 
