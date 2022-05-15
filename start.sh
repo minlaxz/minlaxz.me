ec608838185f892cc65746a69220c8116efc06e5
minlaxz <minminlaxz@gmail.com>
2022-05-15 09:59:29 +0630

    update: Webserver

--- start.sh
+++ start.sh
@@ -1,1 +1,4 @@
+#!/bin/sh
+
 docker-compose -f docker-compose.prod.yml exec web python manage.py migrate --noinput
+docker-compose -f docker-compose.prod.yml exec web python manage.py collectstatic --no-input --clear
